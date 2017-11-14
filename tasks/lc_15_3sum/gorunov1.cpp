8c6bd8e4900f832329710420eb597869f8f42572
M Squared <birthofgenesis@yahoo.com>
2017-11-14 12:30:19 +0000

    Update gorunov1.cpp
    Added optimizations. Global namespace was std so there was no need to have the scope resolution of std:: in the code. Removed return 0 because in modern C++ you are not required to do it anymore because it happens by default. Added auto keyword in for loop for modern semantics. It deduces the appropriate datatype on its own.

--- tasks/lc_15_3sum/gorunov1.cpp
+++ tasks/lc_15_3sum/gorunov1.cpp
@@ -3,26 +3,33 @@
 #include <algorithm>
 using namespace std;
 vector<vector<int>> threeSum(vector<int>& nums) {
-    if (nums.size() < 3) return std::vector<vector<int> >(); 
-    std::vector<vector<int> > output;
-    std::sort(nums.begin(), nums.end());//, std::greater<int>());
+    auto nsize = nums.size();
+    if (nsize < 3) return vector<vector<int> >(); 
+    vector<vector<int> > output;
+    auto nbegin = nums.begin();
+    auto nend = nums.end();
+    sort(nbegin, nend);//, std::greater<int>());
     do {
-        if (nums[nums.size()-1] + nums[nums.size()-2] + nums[nums.size()-3] == 0){
-            vector<int> v = {nums[nums.size()-1], nums[nums.size()-2], nums[nums.size()-3]};
-            std::sort(v.begin(),v.end());
-            output.push_back(v);
+        if (nums[nsize-1] + nums[nsize-2] + nums[nsize)-3] == 0){
+            vector<int> v = {nums[nsize-1], nums[nsize)-2], nums[nsize-3]};
+            auto vbegin = v.begin();
+            auto vend = v.end();
+            sort(vbegin,vend);
+            output.emplace_back(v);
         }
-    }while ( std::next_permutation(nums.begin(),nums.end()) );
-    std::sort(output.begin(), output.end());
-    output.erase(std::unique(output.begin(), output.end()), output.end());
+    }while ( next_permutation(nbegin,nend) );
+    auto obegin = output.begin();
+    auto oend = output.end();
+    sort(obegin, oend);
+    output.erase(unique(obegin, oend), oend);
     return output;  
 };
 
-int main(){
+int main(int argc, char* argv[]){
     vector<int> input = {-1,0,1,2,-1,-4};
-    std::vector<std::vector<int> > res = threeSum(input);
-    for (int i =0; i < res.size(); i++){
-        std::cout << res[i][0] << " " << res[i][1] << " " << res[i][2] << "\n";
+    vector<vector<int> > res = threeSum(input);
+    auto rsize = res.size();
+    for (auto i = 0; i < rsize; i++){
+        cout << res[i][0] << " " << res[i][1] << " " << res[i][2] << "\n";
     }
-    return 0;
 }
